76c35c35f900cf8ebd1164cf6da2f9bde07b7055
Prozymax <maxwellprosper76@gmail.com>
2025-09-09 09:51:17 +0100

    Add index.js and update mobile styles
    Introduced js/index.js with basic variable and type examples. Improved mobileview/j.css for better responsiveness, adjusting category container layout and category sizing. Updated index.html to correctly include mobile CSS after other styles.

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// string integers booleans null undefined array objects
+// string: "hello" // string
+// integer: 123
+// floats: 12.34
+// boolean: true // false
+// null
+// undefined
+// Nan - not a number
+// array: [1,2,3,4,5] // list of items
+// objects: {name: "john", age: 30} // key value pairs
+
+var er = 8;
+var e = "8";
+const isTrue = true;
+const isFalse = false;
+
+var sum = er + e;
+
+console.log(typeof (sum), sum, 'Booleans: ', isTrue + isFalse);
+console.log('Booleans: ', isTrue);
